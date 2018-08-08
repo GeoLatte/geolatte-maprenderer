f68b2e8ed17e543d5806f5cbba2919e1d9f1deb6
Natal Vande Casteele <natal@libius.be>
2018-08-08 14:50:44 +0200

    WDB-8040: andere tekening naargelang resolutie

--- src/main/scala/be/wegenenverkeer/mosaic/application/painters/OpstellingImagePainter.scala
+++ src/main/scala/be/wegenenverkeer/mosaic/application/painters/OpstellingImagePainter.scala
@@ -3,25 +3,58 @@ package be.wegenenverkeer.mosaic.application.painters
 import java.awt.BasicStroke
 
 import be.wegenenverkeer.mosaic.domain.model._
-import org.geolatte.maprenderer.map.{ MapGraphics, Painter, PlanarFeature }
+import org.geolatte.maprenderer.map.{MapGraphics, Painter, PlanarFeature}
 import org.geolatte.maprenderer.painters.EmbeddedImagePainter
 import org.geolatte.maprenderer.util.ImageUtils
 
-/*
-  Tekent een volledige opstelling naar een MapGraphics
-  Er wordt een PlanarFeature verwacht met onder getProperties,get("properties) een Opstelling
- */
-class OpstellingImagePainter(graphics: MapGraphics) extends Painter {
+/**
+  *  Tekent een volledige opstelling naar een MapGraphics
+  *  Er wordt een PlanarFeature verwacht met onder getProperties.get("properties) een Opstelling
+  *
+  * if (resolution <= 0.125) {
+  *       return geselecteerd ? opstellingMetAanzichten(feature, geselecteerd, false) : opstellingMetAanzichten(feature, geselecteerd, false);
+  *     } else if (resolution <= 0.25) {
+  *       return geselecteerd ? opstellingMetAanzichten(feature, geselecteerd, true) : opstellingMetAanzichten(feature, geselecteerd, true);
+  *     } else if (resolution <= 0.5) {
+  *       return opstellingMetHoek(feature, geselecteerd);
+  *     } else {
+  *       return opstellingAlsPunt(feature, geselecteerd);
+  *     }
+  *
+  *   @param graphics   the graphics to use
+  *   @param resolution resolutie die bepaalt welke soort voorstelling getekend wordt.
+  *                       Vb: 1 van [1024.0, 512.0, 256.0, 128.0, 64.0, 32.0, 16.0, 8.0, 4.0, 2.0, 1.0, 0.5, 0.25, 0.125, 0.0625, 0.03125]
+  */
+class OpstellingImagePainter(graphics: MapGraphics, resolution: Double) extends Painter {
 
   override def paint(geojsonPlanarFeature: PlanarFeature): Unit = {
-    val opstelling = geojsonPlanarFeature.getProperties.get("properties").asInstanceOf[Opstelling]
+    val opstelling   = geojsonPlanarFeature.getProperties.get("properties").asInstanceOf[Opstelling]
+    val opstellingPF = PlanarFeature.from(new OpstellingFeature(opstelling))
 
-    renderOpstellingHoek(PlanarFeature.from(new OpstellingFeature(opstelling)), opstelling)
+    resolution match {
+      case res if res <= 0.125 => renderOpstellingMetAanzichten(opstellingPF, opstelling, klein = false)
+      case res if res <= 0.25  => renderOpstellingMetAanzichten(opstellingPF, opstelling, klein = true)
+      case res if res <= 0.5   => renderOpstellingMetHoek(opstellingPF, opstelling)
+      case res                 => renderOpstellingAlsPunt(opstellingPF)
+    }
 
-    opstelling.aanzichten.foreach(renderAanzicht(opstelling, _))
   }
 
-  private def renderOpstellingHoek(planarFeature: PlanarFeature, opstelling: Opstelling): Unit = {
+  private def renderOpstellingAlsPunt(planarFeature: PlanarFeature): Unit = {
+    val painter = new PointPainter(
+      graphics,
+      new BasicStroke(Math.round(1.5 / graphics.getMapUnitsPerPixel)),
+      3
+    )
+    painter.paint(planarFeature)
+  }
+
+  private def renderOpstellingMetAanzichten(planarFeature: PlanarFeature, opstelling: Opstelling, klein: Boolean): Unit = {
+    renderOpstellingMetHoek(planarFeature, opstelling)
+    opstelling.aanzichten.foreach(renderAanzicht(_, opstelling, klein))
+  }
+
+  private def renderOpstellingMetHoek(planarFeature: PlanarFeature, opstelling: Opstelling): Unit = {
     val painter = new ImagePainter(
       graphics,
       feature => ImageUtils.readImageFromBase64String(opstelling.binaireData.kaartvoorstelling.data)
@@ -29,10 +62,14 @@ class OpstellingImagePainter(graphics: MapGraphics) extends Painter {
     painter.paint(planarFeature)
   }
 
-  private def renderAanzicht(opstelling: Opstelling, aanzicht: Aanzicht): Unit = {
+  private def renderAanzicht(aanzicht: Aanzicht, opstelling: Opstelling, klein: Boolean): Unit = {
+    val aanzichtImg = ImageUtils.readImageFromBase64String(
+      if (klein) aanzicht.binaireData.platgeslagenvoorstellingklein.data  else aanzicht.binaireData.platgeslagenvoorstelling.data
+    )
+
     val painter = new EmbeddedImagePainter(
       graphics,
-      feature => ImageUtils.readImageFromBase64String(aanzicht.binaireData.platgeslagenvoorstelling.data),
+      feature => aanzichtImg,
       feature => aanzicht.anker.asGeolattePoint(),
       feature => aanzicht.hoek * -1,
       new BasicStroke(Math.round(2 / graphics.getMapUnitsPerPixel))
--- src/test/scala/be/wegenenverkeer/mosaic/test/painter/OpstellingImagePainterTest.scala
+++ src/test/scala/be/wegenenverkeer/mosaic/test/painter/OpstellingImagePainterTest.scala
@@ -17,7 +17,7 @@ import org.geolatte.maprenderer.map.PlanarFeature
 import org.geolatte.test.TestSupport.assertImageEquals
 
 import collection.JavaConverters._
-import scala.io.{ BufferedSource, Source }
+import scala.io.Source
 
 class OpstellingImagePainterTest extends FunSuite {
 
@@ -37,59 +37,10 @@ class OpstellingImagePainterTest extends FunSuite {
     override def getProperties: util.Map[String, AnyRef] = Map("properties" -> geoJson.properties.asInstanceOf[AnyRef]).asJava
   }
 
-  test("Rendering opstellingen voorbeeld 1") {
-
-    val bufferedSource = Source.fromURL(getClass.getResource("/opstellingen.chunked.1.json"))
-
-    val geoJsons = bufferedSource.getLines.map(line => {
-      Json.parse(line).validate[GeoJson] match {
-        case JsSuccess(opstelling, _) => opstelling
-        case e: JsError               => sys.error("Parse error: " + JsError.toJson(e).toString())
-      }
-    })
-
-    val extent      = new Envelope[C2D](new C2D(153018, 208719), new C2D(153443, 209124), CRS.LAMBERT72)
-    val dim         = new Dimension(1800, 1600)
-    val mapGraphics = new AWTMapGraphics(dim, extent)
-    val painter     = new OpstellingImagePainter(mapGraphics)
-
-    geoJsons.foreach(geoJson => painter.paint(geoJson.asPlanarFeature()))
-
-    bufferedSource.close
-
-    ImageIO.write(mapGraphics.renderImage, "PNG", new File("/tmp", "verkeersborden 1.png"))
-  }
-
   // https://apps.mow.vlaanderen.be/geolatte-nosqlfs/api/databases/featureserver/verkeersborden/query?bbox=152911.5,209130.8125,153212.5,209330.9375
-  test("Rendering opstellingen voorbeeld 2") {
+  test("Visuele verificatie opstelling verschillende resoluties - zie /tmp") {
 
-    val bufferedSource = Source.fromURL(getClass.getResource("/opstellingen.chunked.2.json"))
-
-    val geoJsons = bufferedSource.getLines.map(line => {
-      Json.parse(line).validate[GeoJson] match {
-        case JsSuccess(opstelling, _) => opstelling
-        case e: JsError               => sys.error("Parse error: " + JsError.toJson(e).toString())
-      }
-    })
-
-    val extent      = new Envelope[C2D](new C2D(152911.5, 209130.8125), new C2D(153212.5, 209330.9375), CRS.LAMBERT72)
-    val dim         = new Dimension(1800, 1600)
-    val mapGraphics = new AWTMapGraphics(dim, extent)
-    val painter     = new OpstellingImagePainter(mapGraphics)
-
-    geoJsons.foreach(geoJson => painter.paint(geoJson.asPlanarFeature()))
-
-    bufferedSource.close
-
-    ImageIO.write(mapGraphics.renderImage, "PNG", new File("/tmp", "verkeersborden 2.png"))
-  }
-
-  // https://wms2.apps.mow.vlaanderen.be/geowebcache/service/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image%2Fpng&
-  // TRANSPARENT=true&LAYERS=dienstkaart-grijs&TILED=true&SRS=EPSG%3A31370&WIDTH=256&HEIGHT=256&STYLES=&BBOX=152976%2C209159.75%2C153008%2C209191.75
-
-  test("Tiles") {
-
-    val bufferedSource = Source.fromURL(getClass.getResource("/opstellingen.chunked.2.json"))
+    val bufferedSource = Source.fromURL(getClass.getResource("/opstellingen.chunked.json"))
 
     val geoJsons = bufferedSource.getLines
       .map(line => {
@@ -99,6 +50,44 @@ class OpstellingImagePainterTest extends FunSuite {
         }
       })
       .toList
+
+    bufferedSource.close
+
+    val tileSize = 256 * 6
+    val gridSize = 32
+    val extent = new Envelope[C2D](new C2D(152911.5, 209130.8125),
+                                   new C2D(153212.5, 209330.9375),
+                                   CRS.LAMBERT72)
+    val dim         = new Dimension(1536, 1536)
+
+    def tekenOpstellingen(resolutie: Double): Unit = {
+      val mapGraphics = new AWTMapGraphics(dim, extent)
+      val painter     = new OpstellingImagePainter(mapGraphics, resolutie)
+      geoJsons.foreach(geoJson => painter.paint(geoJson.asPlanarFeature()))
+      val img = mapGraphics.renderImage
+      ImageIO.write(img, "PNG", new File("/tmp", s"verkeersborden - resolutie $resolutie.png"))
+    }
+
+    Seq(0.125, 0.25, 0.5, 1.0).foreach(tekenOpstellingen)
+  }
+
+  // https://wms2.apps.mow.vlaanderen.be/geowebcache/service/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image%2Fpng&
+  // TRANSPARENT=true&LAYERS=dienstkaart-grijs&TILED=true&SRS=EPSG%3A31370&WIDTH=256&HEIGHT=256&STYLES=&BBOX=152976%2C209159.75%2C153008%2C209191.75
+
+  test("Tiles resolutie 0.125") {
+
+    val bufferedSource = Source.fromURL(getClass.getResource("/opstellingen.chunked.json"))
+
+    val geoJsons = bufferedSource.getLines
+      .map(line => {
+        Json.parse(line).validate[GeoJson] match {
+          case JsSuccess(opstelling, _) => opstelling
+          case e: JsError               => sys.error("Parse error: " + JsError.toJson(e).toString())
+        }
+      })
+      .toList
+
+    bufferedSource.close
 
     val aantalTiles = 4
 
@@ -112,7 +101,9 @@ class OpstellingImagePainterTest extends FunSuite {
       for (y <- origin.last to (origin.last + gridSize * aantalTiles) by gridSize) {
         val (minX, minY, maxX, maxY) = (x, y, x + gridSize, y + gridSize)
 
-        val img = generateTile(minX, minY, maxX, maxY, bufferedSource, geoJsons)
+        val img = generateTile(minX, minY, maxX, maxY, geoJsons, tileSize)
+
+        // ImageIO.write(img, "PNG", new File("/tmp", s"tile-$minX-$minY-$maxX-$maxY.png"))
 
         // TODO -- why is this necessary here?
         // we first write the rendered file to disk, because writing to disk and then reading might cause slight
@@ -120,28 +111,27 @@ class OpstellingImagePainterTest extends FunSuite {
         val tmpFile = File.createTempFile("tmp", "png")
         ImageIO.write(img, "PNG", tmpFile)
         val received = ImageIO.read(tmpFile)
+
         assertImageEquals(s"tile-$minX-$minY-$maxX-$maxY-expected.png", received)
       }
     }
   }
 
-//  https://wms3.apps.mow.vlaanderen.be/geowebcache/service/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image%2Fpng&TRANSPARENT=true&LAYERS=dienstkaart-grijs&
-  // TILED=true&SRS=EPSG%3A31370&WIDTH=256&HEIGHT=256&STYLES=&BBOX=152944%2C209159.75%2C152976%2C209191.75
+  //  https://wms3.apps.mow.vlaanderen.be/geowebcache/service/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image%2Fpng&TRANSPARENT=true&LAYERS=dienstkaart-grijs&
+  //  TILED=true&SRS=EPSG%3A31370&WIDTH=256&HEIGHT=256&STYLES=&BBOX=152944%2C209159.75%2C152976%2C209191.75
   private def generateTile(minX: Double,
                            minY: Double,
                            maxX: Double,
                            maxY: Double,
-                           bufferedSource: BufferedSource,
-                           geoJsons: List[GeoJson]): RenderedImage = {
+                           geoJsons: List[GeoJson],
+                           tileSize: Int): RenderedImage = {
 
     val extent      = new Envelope[C2D](new C2D(minX, maxY), new C2D(maxX, minY), CRS.LAMBERT72)
-    val dim         = new Dimension(256, 256)
+    val dim         = new Dimension(tileSize, tileSize)
     val mapGraphics = new AWTMapGraphics(dim, extent)
-    val painter     = new OpstellingImagePainter(mapGraphics)
+    val painter     = new OpstellingImagePainter(mapGraphics, (maxX - minX) / tileSize)
 
     geoJsons.foreach(geoJson => painter.paint(geoJson.asPlanarFeature()))
-
-    bufferedSource.close
 
     mapGraphics.renderImage
   }
